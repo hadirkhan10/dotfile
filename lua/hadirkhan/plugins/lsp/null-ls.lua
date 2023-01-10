a99320ff24808ae181a4823e383713d701f74c9f
Hadir Khan <hadirkhan10@gmail.com>
2023-01-10 13:01:46 -0800

    new config using lsp and lua

---
+++ lua/hadirkhan/plugins/lsp/null-ls.lua
@@ -0,0 +1,39 @@
+-- import null-ls plugin safely
+local setup, null_ls = pcall(require, "null-ls")
+if not setup then
+	return
+end
+
+-- for conciseness
+local formatting = null_ls.builtins.formatting -- to setup formatters
+local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+
+-- to setup format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup({
+	sources = {
+		diagnostics.pylint,
+		formatting.clang_format,
+		formatting.stylua,
+	},
+	-- configure format on save
+	on_attach = function(current_client, bufnr)
+		if current_client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({
+						filter = function(client)
+							--  only use null-ls for formatting instead of lsp server
+							return client.name == "null-ls"
+						end,
+						bufnr = bufnr,
+					})
+				end,
+			})
+		end
+	end,
+})
---
+++ lua/hadirkhan/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+-- import nvim-treesitter plugin safely
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+-- configure treesitter
+treesitter.setup({
+	-- enable syntax highlighting
+	highlight = {
+		enable = true,
+	},
+	-- enable indentation
+	indent = { enable = true },
+	-- enable autotagging (w/ nvim-ts-autotag plugin)
+	autotag = { enable = true },
+	-- ensure these language parsers are installed
+	ensure_installed = {
+		"json",
+		"yaml",
+		"markdown",
+		"bash",
+		"lua",
+		"vim",
+		"python",
+		"c",
+		"cpp",
+		"verilog",
+		"dockerfile",
+		"gitignore",
+	},
+	-- auto install above language parsers
+	auto_install = true,
+})
